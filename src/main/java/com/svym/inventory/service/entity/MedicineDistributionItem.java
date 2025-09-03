254acd57bf649a5fb92f5c8ee8584781dec71e5e
raneeshakdev <76868830+raneeshakdev@users.noreply.github.com>
2025-09-03 21:20:07 +0530

    Renamed the table name

--- src/main/java/com/svym/inventory/service/entity/MedicineDistributionItem.java
+++ src/main/java/com/svym/inventory/service/entity/MedicineDistributionItem.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "medicina_distribution_items")
+@Table(name = "medicine_distribution_items")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
